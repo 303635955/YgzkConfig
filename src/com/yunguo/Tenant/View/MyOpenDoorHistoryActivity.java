56ea800508b3b9f546e8c916a282b3116de06cef
yzq <1334279239@qq.com>
2016-06-11 15:59:52 +0800

    ,2次更新

---
+++ src/com/yunguo/Tenant/View/MyOpenDoorHistoryActivity.java
@@ -0,0 +1,15 @@
+package com.yunguo.Tenant.View;
+
+import com.yunguo.Tenant.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyOpenDoorHistoryActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.history_open_activity);
+	}
+}
---
+++ src/com/yunguo/Tenant/View/MySwingHistoryActivity.java
@@ -0,0 +1,15 @@
+package com.yunguo.Tenant.View;
+
+import com.yunguo.Tenant.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MySwingHistoryActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.history_swingcard_activity);
+	}
+}
